4861c00696b388e298667abe1798cf7fa7b38117
seipan <yamasakipann0218@gmail.com>
2024-07-31 02:23:58 +0900

    remove test.go to .go

--- requester.go
+++ requester.go
@@ -1,1 +1,13 @@
 package requester
+
+import "net/url"
+
+type Requester struct {
+	// ...
+
+	URL url.URL
+}
+
+func NewRequester() *Requester {
+	return &Requester{}
+}
--- requester_test.go
+++ requester_test.go
@@ -1,13 +1,1 @@
 package requester
-
-import "net/url"
-
-type Requester struct {
-	// ...
-
-	URL url.URL
-}
-
-func NewRequester() *Requester {
-	return &Requester{}
-}
